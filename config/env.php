8337c4a3703f2029d4e120889554cf29262a1d0d
ilhooq <contact@sphilip.com>
2016-04-20 23:36:11 +0200

    Change env.php

--- config/env.php
+++ config/env.php
@@ -1,12 +1,14 @@
 <?php
 use M1\Env\Parser;
 
-if (file_exists(__DIR__.'/../../.env')) {
-    $env = Parser::parse(file_get_contents(__DIR__.'/../../.env'));
+defined('APP_BASE_PATH') or die('APP_BASE_PATH constant must be defined.');
+
+if (file_exists(APP_BASE_PATH.'/.env')) {
+    $env = Parser::parse(file_get_contents(APP_BASE_PATH.'/.env'));
     foreach ($env as $key => $value) {
         putenv("{$key}={$value}");
     }
 }
 
 defined('YII_DEBUG') or define('YII_DEBUG', (boolean) getenv('YII_DEBUG'));
-defined('YII_ENV') or define('YII_ENV', getenv('YII_ENV'));
+defined('YII_ENV') or define('YII_ENV', getenv('YII_ENV')? getenv('YII_ENV') : 'prod');
